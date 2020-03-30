39caf3ef44b0a59ecae7ffbe28341d5eb8164012
neeerajtk <neerajtk560@gmail.com>
2020-03-30 13:56:25 +0000

    ejs files added

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 var express = require("express");
 var app = express();
 var request = require("request");
-
+app.set("view engine","ejs");
 app.get("/results", function(req, res){
 	request("http://www.omdbapi.com/?s=guardians+of+the+galaxy&apikey=thewdb",function(error,response,body){
 		if(!error && response.statusCode==200)
 			{
-				var results = JSON.parse(body);
-				res.send(results["Search"]);
+				var data = JSON.parse(body);
+				res.render("results",{data: data});
 			}
 	});
 });
